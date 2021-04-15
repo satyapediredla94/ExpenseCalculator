3a436ac22f041610a27f55d449eb1eb15b6ce7f2
Satya Pediredla <bharathpsd94@gmail.com>
2021-04-14 20:32:11 -0500

    Added Navigation Drawer along with navigation architecture

--- app/src/main/java/com/example/expensecalculator/MainActivity.kt
+++ app/src/main/java/com/example/expensecalculator/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.expensecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        findViewById<NavigationView>(R.id.nav_view)
+                .setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
